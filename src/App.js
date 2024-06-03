4fd8fafaf31cf926b7e88b0c419399c644d8b29d
rout3tecnologia <rout3tecnologia@gmail.com>
2024-06-03 13:30:55 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code> Soluções de Redes para ISPs e B2B</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.celeti.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Empresa parceira da @celetioficial
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
